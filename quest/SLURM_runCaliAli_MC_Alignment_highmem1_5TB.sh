d9b953df6e1aa0b0d5afd3771759694d7a146bf0
John Marshall <jjm2128@gmail.com>
2026-03-19 11:12:32 -0500

    changes from quest

---
+++ quest/SLURM_runCaliAli_MC_Alignment_highmem1_5TB.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -A p30771
+#SBATCH -p genhimem
+#SBATCH -t 48:00:00
+#SBATCH -o ./logfiles/CaliAli_dsMCAlignment.%x-%j.out # STDOUT
+#SBATCH --job-name="CaliAli_Alignment_cpuspertask"
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=1400G
+
+module purge all
+
+cd ~
+
+#path to file 
+
+INPUT_combinedDir=$1
+
+
+echo $INPUT_combinedDir
+
+#add project directory to PATH
+export PATH=$PATH/projects/p30771/
+
+
+#load modules to use
+module load gstreamer/1.20
+module load matlab/r2023b
+
+#cd to script directory
+cd /home/jma819/CaliAli_quest
+#run analysis 
+
+matlab -nosplash -nodesktop -r "addpath(genpath('/home/jma819/CaliAli_quest')); \
+n=str2double(getenv('SLURM_CPUS_PER_TASK')); \
+if isnan(n) || n<1, n=str2double(getenv('SLURM_NPROCS')); end; \
+if isnan(n) || n<1, n=feature('numcores'); end; \
+maxNumCompThreads(n); \
+dsPath='$INPUT_combinedDir';run('runCaliAli_MCAlignment.m');exit;"
+
+echo 'finished analysis'
